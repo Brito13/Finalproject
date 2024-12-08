c5b32524a7ec74732bd7453c58317891a52101f2
John <86134053+JohnAGonzalez31@users.noreply.github.com>
2024-12-07 21:06:27 -0400

    Update main.java

--- Projecto/src/main/java/com/example/projecto/main.java
+++ Projecto/src/main/java/com/example/projecto/main.java
@@ -1,17 +1,24 @@
 package com.example.projecto;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+import java.awt.*;
 
 public class main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        primaryStage.setTitle("Mi Aplicación");
+        primaryStage.setTitle("SmartManager");
+        Image icon = new Image("C:\\Users\\John\\Finalproject\\Projecto\\src\\main\\resources\\com\\example\\projecto\\NextByte.png");
+        primaryStage.getIcons().add(icon);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
